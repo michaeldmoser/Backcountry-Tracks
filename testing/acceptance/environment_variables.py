04cdfe7d1bd7dec6b706fc305d39146601fce790
Michael Moser <michaeldmoser@gmail.com>
2011-06-11 07:57:12 -0500

    * Tweaked User Registration acceptance test to not expect a new page after submitting the form  * Browser open now opens using an environment variable  * Make layout of environment_variables.py more readable long term  * Replaced static content in tests with variables. See the file testing/acceptance/environment_variables.py

---
+++ testing/acceptance/environment_variables.py
@@ -0,0 +1,10 @@
+import socket
+from tptesting import environment
+
+environ = environment.create()
+
+SITE_HOME_PAGE = 'http://%s/' % socket.getfqdn()
+ALBERT = environ.albert
+DOUGLAS = environ.douglas
+RAMONA = environ.ramona
+SELENIUM_SERVER = environ.selenium_server
