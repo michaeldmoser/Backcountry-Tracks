3efbab13c1eec6496e0dcd858c2e73c348a223b9
Michael Moser <michaeldmoser@gmail.com>
2011-06-12 10:49:12 -0500

    Remove unused pika fake classes

--- testing/TripPlannerTestingLib/tptesting/fakepika.py
+++ testing/TripPlannerTestingLib/tptesting/fakepika.py
@@ -1,19 +1,6 @@
 from pika import frame, spec
 import logging
 
-class PikaChannelFake(object):
-    def basic_publish(self, exchange=None, routing_key=None, body=None, properties=None):
-        self.message_body = body
-
-class PikaConnectionFake(object):
-    def __init__(self, params, on_open_callback=None):
-        if on_open_callback is not None:
-            on_open_callback(self)
-
-    def channel(self, callback=None):
-        if callback is not None:
-            callback(PikaChannelFake())
-
 class PikaMessage(object):
     def __init__(self, exchange, routing_key, body, properties,
             mandatory, immediate):
