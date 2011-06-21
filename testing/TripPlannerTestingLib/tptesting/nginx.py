d6722903323a7dadb8c46b1f9efad07040622803
Michael Moser <michaeldmoser@gmail.com>
2011-06-21 17:00:55 -0500

    Fix typos

--- testing/TripPlannerTestingLib/tptesting/nginx.py
+++ testing/TripPlannerTestingLib/tptesting/nginx.py
@@ -4,18 +4,18 @@ class NginxEnvironment(object):
 
     def __init__(self, environment):
         self.environ = environment
-        self.devnull = sefl.environ.devull
+        self.devnull = self.environ.devnull
 
     def start(self):
         '''
         Start nginx
         '''
-        subprocess.call(['/etc/init.d/nginx', 'start'], stdout=self.devnull
+        subprocess.call(['/etc/init.d/nginx', 'start'], stdout=self.devnull,
                 stderr=self.devnull)
 
     def stop(self):
         '''
         Stop nginx
         '''
-        subprocess.call(['/etc/init.d/nginx', 'stop'], stdout=self.devnull
+        subprocess.call(['/etc/init.d/nginx', 'stop'], stdout=self.devnull,
                 stderr=self.devnull)
