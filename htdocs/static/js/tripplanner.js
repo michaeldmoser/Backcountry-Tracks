a9487743679d9312bba966b55d643e65751a9fd1
Paul Lexen <paul.lexen@gmail.com>
2011-06-22 23:42:51 +0000

    Made following changes:  * Added jquery-ui "thank you" dialog to registration  * Fixed tpRegistration plugin spelling  * Updated tpRegistration to use closures  * Updated test_registration.js tests  * Added Registration plugin to register.html

--- htdocs/static/js/tripplanner.js
+++ htdocs/static/js/tripplanner.js
@@ -4,15 +4,26 @@
 
 (function( $ ){
 
-  $.fn.tpRegisration = function() {
-    this.submit($.fn.tpRegisration.submitForm);
+  $.fn.tpRegistration = function(options) {
+
+    $this = this
+
+    $this.options = $.extend({
+      thankYouElement: null
+    }, options);
+
+    this.submit(function(event) {
+      $.fn.tpRegistration.submitForm.call($this, event)
+    });
     return this;
   }
 
-  $.fn.tpRegisration.ajax = $.ajax;
+  $.fn.tpRegistration.ajax = $.ajax;
 
-  $.fn.tpRegisration.submitForm = function(event) {
+  $.fn.tpRegistration.submitForm = function(event) {
     event.preventDefault();
+
+    $this = this;
 
     // get JSON encoded form values
     var values = {};
@@ -22,12 +33,21 @@
 
     var data = JSON.stringify(values);
 
-    $.fn.tpRegisration.ajax(this.action, {
+    $.fn.tpRegistration.ajax($(this).attr('action'), {
       type: 'POST',
       data: data,
-      contentType: 'application/json'
+      contentType: 'application/json',
+      success: function(data, textStatus, jqXHR) {
+        $.fn.tpRegistration.handleSuccess.call($this, data, textStatus, jqXHR)
+      }
     }, 'json');
   };
+
+  $.fn.tpRegistration.handleSuccess = function(data, textStatus, jqXHR) {
+    if (this.options.thankYouElement) {
+      this.options.thankYouElement.dialog();
+    }
+  }
 
 })( jQuery );
 
--- testing/javascript/public/unit/test_registration.js
+++ testing/javascript/public/unit/test_registration.js
@@ -1,11 +1,14 @@
 testHelpers = {
-    submitForm: $.fn.tpRegisration.submitForm,
-    ajax: $.fn.tpRegisration.ajax,
+    submitForm: $.fn.tpRegistration.submitForm,
+    ajax: $.fn.tpRegistration.ajax,
 
     reset: function() {
-      $.fn.tpRegisration.submitForm = testHelpers.submitForm;
-      $.fn.tpRegisration.ajax = testHelpers.ajax;
+      $.fn.tpRegistration.submitForm = testHelpers.submitForm;
+      $.fn.tpRegistration.ajax = testHelpers.ajax;
+      $.dialog = testHelpers.dialog;
+
       testHelpers.createForm()
+      testHelpers.createThankYouElement()
     },
 
     createForm: function() {
@@ -16,6 +19,10 @@ testHelpers = {
           '<input name="test3" value="3" />',
         '</form>'
       ].join('\n'));
+    },
+
+    createThankYouElement: function() {
+      testHelpers.thankYouElement =  $('<div>Thank You</div>');
     }
 }
 
@@ -27,67 +34,70 @@ module("Registration Tests", {
 
 test("test trigger submit calls submit handler", function() {
   var callsSubmitHandler = false;
-  $.fn.tpRegisration.submitForm = function(event) {
+  $.fn.tpRegistration.submitForm = function(event) {
     callsSubmitHandler = true;
   }
 
-  testHelpers.form.tpRegisration().trigger("submit");
+  testHelpers.form.tpRegistration().trigger("submit");
   ok(callsSubmitHandler, "submit handler called");
 });
 
 test("test submit form sends ajax request", function() {
   var sendsAjax = false;
-  $.fn.tpRegisration.ajax = function(url, options) {
+  $.fn.tpRegistration.ajax = function(url, options) {
     sendsAjax = true;
   }
 
-  testHelpers.form.tpRegisration().trigger("submit");
+  testHelpers.form.tpRegistration().trigger("submit");
   ok(sendsAjax, "ajax function called");
 });
 
 test("test ajax url follows form action", function() {
-  $.fn.tpRegisration.ajax = function(url, options) {
-    ok(url.indexOf('/test') > 0)
+  var testUrl = '';
+  $.fn.tpRegistration.ajax = function(url, options) {
+    testUrl = url;
   }
-  testHelpers.form.tpRegisration().trigger("submit");
-
-  testHelpers.reset()
+  testHelpers.form.tpRegistration().trigger("submit");
+  ok(testUrl.indexOf('/test') >= 0)
 
   testHelpers.form.attr('action', '/another/test');
-  $.fn.tpRegisration.ajax = function(url, options) {
-    ok(url.indexOf('/another/test') > 0)
-  }
-  testHelpers.form.tpRegisration().trigger("submit");
+  testHelpers.form.tpRegistration().trigger("submit");
+  ok(testUrl.indexOf('/another/test') >= 0)
 });
 
-test("test ajax sends content as application/json", function() {
+test("test ajax sends json-encoded form as application/json", function() {
   var contentType = null;
-  $.fn.tpRegisration.ajax = function(url, options) {
+  var data = null;
+  $.fn.tpRegistration.ajax = function(url, options) {
     options.beforeSend = function(jqXHR, settings) {
       contentType = settings.contentType;
-      return false;
-    }
-    $.ajax(url, options);
-  }
-  testHelpers.form.tpRegisration().trigger("submit");
-
-  //for some reason jsTestDriver breaks on equal()
-  //use ok for now until a fix is found
-  //equal(contentType, 'application/json');
-  ok(contentType === 'application/json')
-});
-
-test("test ajax sends form as json", function() {
-  var data = null;
-  $.fn.tpRegisration.ajax = function(url, options) {
-    options.beforeSend = function(jqXHR, settings) {
       data = settings.data;
       return false;
     }
     $.ajax(url, options);
   }
-  testHelpers.form.tpRegisration().trigger("submit");
+  testHelpers.form.tpRegistration().trigger("submit");
 
-  //equal(data, '{"test1":"1","test2":"2","test3":"3"}');
-  ok(data === '{"test1":"1","test2":"2","test3":"3"}')
+  //for some reason jsTestDriver breaks on equal()
+  //use ok for now until a fix is found
+  ok(contentType === 'application/json', 'data sent as application/json')
+  ok(data === '{"test1":"1","test2":"2","test3":"3"}', 'form data serialized as expected')
+});
+
+test("test handle success method creates thank you modal", function() {
+  $.fn.tpRegistration.ajax = function(url, options) {
+    options.success({}, 'success', {})
+  }
+
+  var isOpen = false;
+  testHelpers.thankYouElement.bind('dialogopen', function(event, ui) {
+    isOpen = true;
+  });
+
+  testHelpers.form.tpRegistration({
+    thankYouElement: testHelpers.thankYouElement
+  }).trigger("submit");
+
+  testHelpers.thankYouElement.dialog('close');
+  ok(isOpen, 'thank you dialog opened')
 });
