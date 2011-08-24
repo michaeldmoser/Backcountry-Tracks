892b8fcb5d3e686c067ab91d069b4e2396f697e4
Michael Moser <michaeldmoser@gmail.com>
2011-08-24 13:39:56 -0500

    Add a new TrailHead testing utility for completely setting up a RequestHandler for testing

--- services/TrailHead/trailhead/tests/utils.py
+++ services/TrailHead/trailhead/tests/utils.py
@@ -1,6 +1,8 @@
 from tptesting import faketornado, environment, fakepika
 from trailhead.mq import PikaClient
 
+import Cookie
+
 def create_fake_application():
     pika_connection_class = fakepika.SelectConnectionFake()
     application = faketornado.WebApplicationFake()
@@ -10,4 +12,31 @@ def create_fake_application():
     pika_connection_class.ioloop.start()
 
     return application, pika_connection_class
+
+def make_user_authenticated(handler, user):
+    handler.set_secure_cookie('user', user)
+    cookie_value = handler._new_cookies[0]
+    handler._cookies = Cookie.BaseCookie()
+    handler._cookies['user'] = cookie_value['user'].value
+
+def setup_handler(Handler, method, url, user=None):
+    '''
+    Setup a RequestHandler so as to be able to simulate HTTP Requests on the object.
+
+    @Handler is the RequestHandler sub-class.
+    @method is the intend HTTP request method (GET, POST, etc...)
+    @url is the URL the request is happening on
+    @user is the email address of the user to simulate as authenticated. If no user is supplied then
+        the request will be treated as unauthenticated.
+    '''
+    application, pika = create_fake_application()
+
+    request = faketornado.HTTPRequestFake(method, url)
+    handler = Handler(application, request)
+    handler._transforms = []
+    if user is not None:
+        make_user_authenticated(handler, user)
+
+    return handler, application, pika
+
 
