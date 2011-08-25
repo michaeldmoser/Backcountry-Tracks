9332b01f6df548166381e0fb2d7d4f550afcb617
Michael Moser <michaeldmoser@gmail.com>
2011-08-25 17:46:34 -0500

    Add debug logging into TrailHead.login

--- services/TrailHead/trailhead/login.py
+++ services/TrailHead/trailhead/login.py
@@ -3,6 +3,8 @@ import uuid
 import json
 from tornado import web
 
+import logging
+
 class BaseHandler(web.RequestHandler):
     def get_current_user(self):
         return self.get_secure_cookie("user")
@@ -23,6 +25,7 @@ class LoginHandler(BaseHandler):
                 reply_to = self.application.mq.rpc_reply,
                 )
         mq.register_rpc_reply(correlation_id, self.respond_to_login)
+        logging.debug('Sending login message for %s' % self.request.body)
         mq.channel.basic_publish(
                 exchange = 'adventurer',
                 routing_key = 'adventurer.login',
@@ -31,6 +34,7 @@ class LoginHandler(BaseHandler):
                 )
 
     def respond_to_login(self, headers, body):
+        logging.debug('Got response for %s' % body)
         reply = json.loads(body)
         if reply['successful'] == True:
             self.set_secure_cookie("user", reply['email'])
