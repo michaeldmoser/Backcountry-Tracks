d375c24229637ddb8c7493db612846b250f214a9
Michael Moser <michaeldmoser@gmail.com>
2011-09-01 10:43:07 -0500

    Make the add_item() more useful

--- testing/TripPlannerTestingLib/tptesting/gear.py
+++ testing/TripPlannerTestingLib/tptesting/gear.py
@@ -6,16 +6,21 @@ class Gear(object):
         self.geardb = self.environ.riak.get_database('personal_gear')
 
 
-    def add_item(self, user, name, description, weight):
+    def add_item(self, owner=None, name=None, description=None, weight=None):
         """
         Add a new piece of gear to the user's inventory
         """
+        assert owner is not None, "You must provide a owner"
+        assert name is not None, "You must provide a name for the gear"
+        assert description is not None, "You must provide a description for the gear"
+        assert weight is not None, "You must provide a weight for the gear"
+
         gear_id = str(uuid.uuid4())
         gear_item = {
                 'name': name,
                 'description': description,
                 'weight': weight,
-                'owner': user,
+                'owner': owner,
                 }
         new_gear = self.geardb.new(gear_id, data=gear_item)
         new_gear.store()
