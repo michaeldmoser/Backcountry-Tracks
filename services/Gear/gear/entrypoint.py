a9f678420b14cedb9a80a745cdf1ec605ee0c194
Michael Moser <michaeldmoser@gmail.com>
2011-10-04 10:08:03 -0500

    Gear service's entrypoint should use the bctplugins entrypoint class

--- services/Gear/gear/entrypoint.py
+++ services/Gear/gear/entrypoint.py
@@ -1,39 +1,7 @@
-import pika
-import json
+from bctplugins import entrypoint
 
+from gear.usergear import UserGear
 
-class GearEntryPoint(object):
-
-    def __init__(self, configuration, environ):
-        self.environ = environ
-        self.config = configuration
-
-        self.usergear = self.__usergear()
-
-
-    def __usergear(self):
-        from gear.usergear import UserGear
-        return UserGear
-
-    def __service(self):
-        from bctmessaging.services import MessagingServiceController
-        return MessagingServiceController
-
-    def __riak(self):
-        from riak import RiakClient
-        return RiakClient
-
-    def start(self):
-        self.environ.open_messaging_channel(self.on_channel_opened)
-
-    def on_channel_opened(self, channel):
-        self.channel = channel
-        service = self.__service()
-
-        riakclient = self.__riak()
-        riak = riakclient(host=self.config['database']['host'])
-        usergear = self.usergear(riak, self.config['database']['bucket'])
-
-        self.service = service(self.channel, self.config, usergear)
-        self.service.start()
+class GearEntryPoint(entrypoint.EntryPoint):
+    service = UserGear
 
