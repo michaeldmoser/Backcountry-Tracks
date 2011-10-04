b47c949c0aef247833910526d865175cc395c739
Michael Moser <michaeldmoser@gmail.com>
2011-10-04 09:39:42 -0500

    Trips service's entrypoint should use the bctplugins entrypoint class

--- services/Trips/trips/entrypoint.py
+++ services/Trips/trips/entrypoint.py
@@ -1,38 +1,10 @@
+from bctplugins import entrypoint
+from trips.db import TripsDb
 
-class EntryPoint(object):
+class EntryPoint(entrypoint.EntryPoint):
+    service = TripsDb
 
     def __init__(self, configuration, environ):
-        self.environ = environ
-        self.config = configuration
-
-        self.db = self.__db()
-
-
-    def __db(self):
-        from trips.db import TripsDb
-        return TripsDb
-
-    def __service(self):
-        from bctmessaging.services import MessagingServiceController
-        return MessagingServiceController
-
-    def __riak(self):
-        from riak import RiakClient
-        return RiakClient
-
-    def start(self):
-        self.environ.open_messaging_channel(self.on_channel_opened)
-
-    def on_channel_opened(self, channel):
-        self.channel = channel
-        service = self.__service()
-
-        riakclient = self.__riak()
-        riak = riakclient(host=self.config['database']['host'])
-        db = self.db(riak, self.config['database']['bucket'])
-
-        self.service = service(self.channel, self.config, db)
-        self.service.start()
-
+        entrypoint.EntryPoint.__init__(self, configuration, environ)
 
 
