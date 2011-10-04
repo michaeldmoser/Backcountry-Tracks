b4fe73aa2f23f977fa5891a624f95f8d32d2f42c
Michael Moser <michaeldmoser@gmail.com>
2011-10-04 10:11:08 -0500

    Adventurer2 service's entrypoint should use the bctplugins entrypoint class

--- services/Adventurer2/adventurer2/entrypoint.py
+++ services/Adventurer2/adventurer2/entrypoint.py
@@ -1,38 +1,7 @@
-import pika
-import json
+from bctplugins import entrypoint
 
+from .model import AdventurerRepository
 
-class EntryPoint(object):
-
-    def __init__(self, configuration, environ):
-        self.environ = environ
-        self.config = configuration
-
-        self.repository = self.__repository()
-
-    def __repository(self):
-        from .model import AdventurerRepository
-        return AdventurerRepository
-
-    def __service(self):
-        from bctmessaging.services import MessagingServiceController
-        return MessagingServiceController
-
-    def __riak(self):
-        from riak import RiakClient
-        return RiakClient
-
-    def start(self):
-        self.environ.open_messaging_channel(self.on_channel_opened)
-
-    def on_channel_opened(self, channel):
-        self.channel = channel
-        service = self.__service()
-
-        riakclient = self.__riak()
-        riak = riakclient(host=self.config['database']['host'])
-        repository = self.repository(riak, self.config['database']['bucket'])
-
-        self.service = service(self.channel, self.config, repository)
-        self.service.start()
+class EntryPoint(entrypoint.EntryPoint):
+    service = AdventurerRepository
 
