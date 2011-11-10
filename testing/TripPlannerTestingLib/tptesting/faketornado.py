5f383a7453bb0b508ee7a6477fab2154b4a00766
Michael Moser <michaeldmoser@gmail.com>
2011-11-10 15:07:00 -0600

    * Add record_usage utilities to faketornado.WebApplicationFake.  * Change how the settings dictionary is handled

--- testing/TripPlannerTestingLib/tptesting/faketornado.py
+++ testing/TripPlannerTestingLib/tptesting/faketornado.py
@@ -8,12 +8,68 @@ class WebApplicationFake(object):
     def __init__(self):
         self.__usage = list()
 
+    def record_usage(self, method, *args, **kwargs):
+        self.__usage.append((method, args, kwargs))
+
+    def verify_usage(self, method, args, kwargs):
+        '''
+        Returns True if method was called and the args and kwargs match the call, 
+        otherwise returns False
+
+        method is function object instance on this class
+        '''
+        for method_call in self.__usage:
+            instancemethod = method_call[0]
+
+            if not callable(instancemethod):
+                continue
+
+            unbound_method = getattr(instancemethod.im_class, instancemethod.__name__)
+            search_unbound_method = getattr(method.im_class, method.__name__)
+            if unbound_method == search_unbound_method:
+                if args != '*' and method_call[1] != args:
+                    return False
+
+                if kwargs != '*' and method_call[2] != kwargs:
+                    return False
+
+                return True
+
+        return False
+
+    def was_called(self, method):
+        '''
+        Returns True if method was called otherwise returns false
+
+        method is function object instance on this class
+        '''
+        for method_call in self.__usage:
+            instancemethod = method_call[0]
+
+            if not callable(instancemethod):
+                continue
+
+            unbound_method = getattr(instancemethod.im_class, instancemethod.__name__)
+            search_unbound_method = getattr(method.im_class, method.__name__)
+            if unbound_method == search_unbound_method:
+                return True
+
+        return False
+
+    @property
+    def usage(self):
+        return self.__usage
+
+###
+### These methods are part of the Pika API, these can/should be used
+### in production code
+###
+
     def __call__(self, handlers=None, default_host='', transforms=None,
             wsgi=False, **settings):
         self.handlers = handlers
         self.default_host = default_host
         self.transforms = [ChunkedTransferEncoding]
-        self.settings = settings
         self.ui_methods = {}
         self.ui_modules = {}
         self._wsgi = wsgi
@@ -21,6 +77,7 @@ class WebApplicationFake(object):
             'cookie_secret': 'test_secret',
             'login_url': '/login'
             }
+        self.settings.update(settings)
 
         return self
 
@@ -31,7 +88,7 @@ class WebApplicationFake(object):
         raise NotImplementedError
 
     def listen(self, port, address='', **kwargs):
-        self.__usage.append((self.listen, (port, address, kwargs)))
+        self.record_usage(self.listen, port, address=address, **kwargs)
 
     def log_request(self, handler):
         pass
@@ -40,11 +97,15 @@ class WebApplicationFake(object):
         raise NotImplementedError
 
 class IOLoopFake(object):
+    def __init__(self):
+        self.timeout_callbacks = list()
+
     def start(self):
-        pass
+        for callback in self.timeout_callbacks:
+            callback()
 
     def add_timeout(self, timeout, connect):
-        connect()
+        self.timeout_callbacks.append(connect)
 
 
 class ioloop(object):
