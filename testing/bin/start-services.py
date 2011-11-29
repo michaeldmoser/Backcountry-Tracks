2bfcbe3577d62a55a6413f8e2d0e087e572b92fd
Michael Moser <michaeldmoser@gmail.com>
2011-11-29 16:00:19 -0600

    start-services.py should create a registered user

--- testing/bin/start-services.py
+++ testing/bin/start-services.py
@@ -2,3 +2,9 @@ from tptesting import environment
 environ = environment.create()
 environ.make_pristine()
 environ.bringup_infrastructure()
+
+douglas = environ.douglas
+douglas.mark_registered()
+environ.create_user()
+
+
