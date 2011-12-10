cb4055225086066800c62175d273b4df082a050d
Michael Moser <michaeldmoser@gmail.com>
2011-12-10 11:02:42 -0600

    Started a "locators" variable file for acceptance tests. Will contain locators for use with the SeleniumLibrary, should make maintenance of acceptance tests easier.

---
+++ testing/acceptance/locators.py
@@ -0,0 +1,6 @@
+
+# Locates a piece of gear in the gear list. The xpath will return the <li> element representing the piece of gear
+GEAR_IN_LIST = """xpath=//div[@id="gear_list_view"]/ul/li/div[contains(., "%s")]/.."""
+
+
+
