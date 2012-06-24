ebc0de5861093c1d7572fa9f92369af225ede902
Michael Moser <michaeldmoser@gmail.com>
2012-06-24 11:33:14 -0500

    Move old tests to the side

---
+++ testing/acceptance_pre-ui-change/environment_variables.py
@@ -0,0 +1,19 @@
+import socket
+from tptesting import environment
+
+environ = environment.create()
+
+SITE_HOME_PAGE = 'http://%s/' % socket.getfqdn()
+
+ALBERT = environ.albert
+DOUGLAS = environ.douglas
+RAMONA = environ.ramona
+
+SELENIUM_SERVER = environ.selenium_server
+
+APPLICATION_HOME_PAGE = SITE_HOME_PAGE + "app/home"
+
+DATA = environ.data
+
+CLOSE_BROWSER = True
+
